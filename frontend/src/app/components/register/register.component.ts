2a4abc59ae01e98e17ae83d4f9177919e20a883c
29574196 <29574196@student.g.nwu.ac.za>
2022-02-13 13:51:40 +0200

    made changes that were required

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -24,6 +24,11 @@ export class RegisterComponent implements OnInit {
       password: '', 
     };
 
+    loginData: any = {
+      email: '',
+      password: '',
+    };
+
 
 
   ngOnInit(): void {
@@ -51,6 +56,47 @@ export class RegisterComponent implements OnInit {
         this.toast.error('Sign-Up Failed, please try again', 'Register');
       }
     );
+  }
+
+
+   // tslint:disable-next-line: typedef
+   login() {
+    
+    this.authService.loginUser(this.loginData).subscribe(
+      (res) => {
+        localStorage.setItem('token', res.token);
+        localStorage.setItem('email', res.email);
+        localStorage.setItem('name', res.name);
+        localStorage.setItem('phone', res.phone);
+        localStorage.setItem('id', res._id);
+        this.reset();
+        console.log('Successful');
+      },
+      (err) => {
+        console.log('Failed');
+        this.toast.error('Login Failed, please try again', 'Login');
+      }
+    );
+  }
+
+   // tslint:disable-next-line: typedef
+   reset() {
+    this.spinner.show();
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.toast.success(
+        `${this.loginData.email} successfully logged in.`,
+        'Welcome back!',
+        {
+          timeOut: 1500,
+          progressBar: true,
+          progressAnimation: 'increasing',
+          positionClass: 'toast-top-right',
+        }
+      );
+      this.spinner.hide();
+    }, 2000);
+    this.router.navigate(['/cart'])
   }
 
 }
